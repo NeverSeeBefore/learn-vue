85cf1b207eb7dcaf2a80f27b9bc368e0410c06f0
chenxiaoxu <chenxiaoxu@daojia-inc.com>
2020-04-06 16:04:37 +0800

    init

--- App.vue
+++ App.vue
@@ -1,28 +1,76 @@
 <template>
   <div>
-    <div>father</div>
-    {{content}}
-    <base-son></base-son>
+    <base-tree :data="data"></base-tree>
   </div>
-  <!-- <div>son</div> -->
-  <!-- <p>{{content}}</p> -->
 </template>
 <script>
-import baseSon from './Son';
+import BaseTree from "./Tree";
 export default {
-  
   components: {
-    'base-son': baseSon
+    "base-tree": BaseTree
   },
-  data(){
+  data() {
     return {
-      content: 'content in father'
-    }
+      data: [
+        {
+          label: "一级 1",
+          children: [
+            {
+              label: "二级 1-1",
+              children: [
+                {
+                  label: "三级 1-1-1"
+                }
+              ]
+            }
+          ]
+        },
+        {
+          label: "一级 2",
+          children: [
+            {
+              label: "二级 2-1",
+              children: [
+                {
+                  label: "三级 2-1-1"
+                }
+              ]
+            },
+            {
+              label: "二级 2-2",
+              children: [
+                {
+                  label: "三级 2-2-1"
+                }
+              ]
+            }
+          ]
+        },
+        {
+          label: "一级 3",
+          children: [
+            {
+              label: "二级 3-1",
+              children: [
+                {
+                  label: "三级 3-1-1"
+                }
+              ]
+            },
+            {
+              label: "二级 3-2",
+              children: [
+                {
+                  label: "三级 3-2-1"
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    };
   }
-}
+};
 </script>
 <style>
-div{
-  background-color: red;
-}
 </style>
