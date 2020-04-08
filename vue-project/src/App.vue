efca872d8a04e3a7891f84f5ba38c8bb75fd6509
chenxiaoxu <chenxiaoxu@daojia-inc.com>
2020-04-08 09:46:12 +0800

    init

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <HelloWorld :level="1">标题</HelloWorld>
+    <base-demo></base-demo>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import BaseDemo from './components/BaseDemo.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    BaseDemo
   }
 }
 </script>
